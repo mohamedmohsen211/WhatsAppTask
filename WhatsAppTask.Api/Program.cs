fd774651e05d93ea970df5687db03ae54716fd67
Mohsen <mohamed.mohsen.21199@gmail.com>
2026-01-21 15:02:27 +0200

    use postgres on railway

--- WhatsAppTask.Api/Program.cs
+++ WhatsAppTask.Api/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddSwaggerGen(c =>
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
